cbff01c09b05d55b7a3a9008018473c567926123
Boldurev1 <aleksandr.boldurev1@gmail.com>
2018-07-15 15:47:02 +0300

    homework5

---
+++ alert.js
@@ -0,0 +1,74 @@
+alert('Hay');
+alert('World');
+var admin, name;//две переменных
+
+name = 'Василий';
+
+admin = name;
+
+alert(admin); // Vasiliy
+
+var land, petr;//две переменных
+
+land ='Земля';
+
+petr ='Петро';
+
+var c,d;// Задача 1
+
+c=15;
+
+d=2;
+
+alert(c+d);
+
+var a,b,c;
+
+a=17;
+
+b=2;
+
+c=5;
+
+alert(a+b+c);
+
+var a,b,c,d;
+
+a=17;
+
+b=10;
+
+c=a-b;
+
+d=7;
+
+alert(c+d);
+
+var str;
+
+str='Hay, World!';
+
+alert(str);
+
+var str1,str2;
+
+str1='Hay,';
+
+str2='World';
+
+alert(str1+str2);
+
+var name;
+
+name='Alex';
+
+alert('Hay,'+name);
+
+var age;
+
+age=25;
+
+alert('Me' + age + 'years');
+
+
+
